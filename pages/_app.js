0121e830442fc3d18d6a3d5f87de64da0a643ccd
Saurabh-Datta <saurabhdatta222001@gmail.com>
2022-07-18 17:29:42 +0530

    Shifted to a new Repository

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react';
+import '../styles/globals.scss';
+import { Layout } from '../components';
+
+function MyApp({ Component, pageProps }) {
+  return (
+  <Layout>
+    <Component {...pageProps} />
+  </Layout>
+  );
+}
+
+export default MyApp
