7aca593b1cd6848c9a4a9aa93eac164da48e1a33
Saurabh-Datta <saurabhdatta222001@gmail.com>
2023-05-11 21:42:53 +0530

    Added Vercel Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import '../styles/globals.scss';
 import { Layout } from '../components';
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
   return (
   <Layout>
     <Component {...pageProps} />
+    <Analytics />
   </Layout>
   );
 }
